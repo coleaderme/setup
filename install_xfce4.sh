c5c644cabfab45aaeaf37b4c3c54532f96796002
vivoiswatching <129755518+vivoiswatching@users.noreply.github.com>
2023-04-03 18:13:30 +0530

    Update and rename xfce4.sh to install_xfce4.sh
    add few more packages

---
+++ install_xfce4.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+sudo pacman -Syy
+sudo pacman -S xorg xorg-server xfce4 gvfs gvfs-mtp ntfs-3g noto-fonts noto-fonts-emoji noto-fonts-cjk alsa-utils xfce4-clipman-plugin viewnior mpv htop p7zip xarchiver
--- xfce4.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-sudo pacman -S xorg xorg-server xfce4 firefox gvfs gvfs-mtp ntfs-3g alsa-utils xfce4-clipman-plugin
