49be9883fa0456b342545ed36cde61f10714340e
vivoiswatching <129755518+vivoiswatching@users.noreply.github.com>
2023-04-03 18:04:28 +0530

    Update s6.sh

--- s6.sh
+++ s6.sh
@@ -3,6 +3,5 @@ pacman -S grub efibootmgr dosfstools mtools
 grub-install --target=x86_64-efi --bootloader-id=grub_efi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd.service
-exit
-umount -lR /mnt
-echo "Everything Done, you can safely reboot now."
+echo "type: exit"
+echo "type: umount -lR /mnt"
