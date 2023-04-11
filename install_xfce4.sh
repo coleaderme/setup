48a2967953d3cfd41f32a956f0e0daa00abad9da
vivoiswatching <129755518+vivoiswatching@users.noreply.github.com>
2023-04-11 14:48:02 +0530

    Update install_xfce4.sh

--- install_xfce4.sh
+++ install_xfce4.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 sudo pacman -Syy
-sudo pacman -S xorg xorg-server xfce4 gvfs gvfs-mtp ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin viewnior mpv htop p7zip xarchiver profile-sync-daemon
+sudo pacman -S xorg xorg-server xfce4 ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin wireguard-tools geany viewnior mpv htop p7zip xarchiver profile-sync-daemon
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
