10ca7601fc1bcae599d9f26085523fb15bda183a
vivoiswatching <129755518+vivoiswatching@users.noreply.github.com>
2023-04-13 01:28:11 +0530

    tldr [man page]

--- install_xfce4.sh
+++ install_xfce4.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 sudo pacman -Syy
-sudo pacman -S xorg xorg-server xfce4 ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin wireguard-tools openresolv geany viewnior aria2 yt-dlp python-brotli mpv htop p7zip xarchiver profile-sync-daemon
+sudo pacman -S xorg xorg-server xfce4 ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin wireguard-tools openresolv geany viewnior aria2 yt-dlp python-brotli mpv htop p7zip xarchiver tealdeer
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
