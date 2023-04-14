3e833f402b447b0662da56592a34b5f8b0b02966
vivoiswatching <129755518+vivoiswatching@users.noreply.github.com>
2023-04-14 21:44:04 +0530

    - removed xfce
    Desktop independent packages

--- install_xfce4.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-sudo pacman -Syy
-sudo pacman -S xorg xorg-server xfce4 ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin wireguard-tools openresolv wgcf geany viewnior aria2 yt-dlp python-brotli python-pip mpv htop p7zip xarchiver tealdeer
-git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
