5192a94546430538ee0f352de603dac23aeaa873
Vivoiswatching <vivoiswatching@gmail.com>
2023-04-14 12:09:53 +0530

    oh-my-bash

--- install_xfce4.sh
+++ install_xfce4.sh
@@ -2,3 +2,4 @@
 sudo pacman -Syy
 sudo pacman -S xorg xorg-server xfce4 ntfs-3g noto-fonts noto-fonts-emoji alsa-utils xfce4-clipman-plugin wireguard-tools openresolv geany viewnior aria2 yt-dlp python-brotli python-pip mpv htop p7zip xarchiver tealdeer
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
--- startx.sh
+++ startx.sh
@@ -4,6 +4,4 @@ sudo mv xinitrc /etc/X11/xinit/xinitrc
 mkdir ~/Pictures
 mv scr.sh ~/Pictures/
 mv mpv ~/.config/
-mv psd.conf ~/.config/psd/
-systemctl --user enable psd.service
-psd preview
+mv /omb/* ~/
