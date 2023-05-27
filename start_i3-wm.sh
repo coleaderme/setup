9518996aff50e8e1e1be38b41c9ab1e3008dc61f
coleaderme <114289459+coleaderme@users.noreply.github.com>
2023-05-28 01:04:38 +0530

    Update start_i3-wm.sh

--- start_i3-wm.sh
+++ start_i3-wm.sh
@@ -4,15 +4,16 @@ sudo pacman -Syy
 sudo pacman -S i3-wm i3status numlockx
 
 #config file
-mv i3 ~/.config/
+mv -rf i3 ~/.config/
 
 #xinit entry
 sudo mv xinitrc_i3 /etc/X11/xinit/xinitrc
 
 mkdir ~/Pictures
 mv scr.sh ~/Pictures/
-mv mpv ~/.config/
+mv -rf mpv ~/.config/
+
 # Custom oh-my-bash config
-mv /omb/* ~/
+mv -rf /omb/* ~/
 mv ~/oh-my-bash ~/.oh-my-bash
 mv ~/bashrc ~/.bashrc
