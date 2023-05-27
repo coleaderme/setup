ccf7211adc82af90ce4ce0d81d693b374fa0b675
coleaderme <114289459+coleaderme@users.noreply.github.com>
2023-05-28 01:09:49 +0530

    Update startx_xfce4.sh

--- startx_xfce4.sh
+++ startx_xfce4.sh
@@ -3,7 +3,7 @@ sudo timedatectl set-ntp true
 sudo mv xinitrc_xfce4 /etc/X11/xinit/xinitrc
 mkdir ~/Pictures
 mv scr.sh ~/Pictures/
-mv mpv ~/.config/
+mv -rf mpv ~/.config/
 mv /omb/* ~/
 mv ~/oh-my-bash ~/.oh-my-bash
 mv ~/bashrc ~/.bashrc
