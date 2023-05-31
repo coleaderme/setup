f1097e22a7ef96ad7baffc2612186bcaf0be9797
vivo <vivoiswatching@gmail.com>
2023-05-31 14:07:07 +0530

    pretty much complete..

--- packages.sh
+++ packages.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 sudo pacman -Syy
-sudo pacman -S xorg-xinit xorg-server ntfs-3g noto-fonts noto-fonts-emoji alsa-utils wireguard-tools wgcf viewnior scrot aria2 yt-dlp python-brotli python-pip mpv htop p7zip tealdeer ripgrep exa profile-sync-daemon fastfetch
+sudo pacman -S xorg-server xorg-xinit ntfs-3g noto-fonts noto-fonts-emoji alsa-utils wireguard-tools wgcf viewnior scrot aria2 yt-dlp python-brotli python-pip mpv htop p7zip tealdeer ripgrep exa profile-sync-daemon fastfetch
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
 yay -S ungoogled-chromium-bin
+
+#config files
 mv psd ~/.config/
-echo "open chromium then run this: systemctl --user enable psd.service"
+mv bashrc ~/.bashrc
+mv mpv ~/.config/
+mkdir ~/Pictures
+
+echo "startx" >> ~/.bash_profile
 echo "echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> ~/.bash_profile
+
+echo "open chromium then run this: systemctl --user enable psd.service"
 echo 'END'
