320f9eb45940b42788c21befd6593b75d6ebf712
coleaderme <coleaderme@gmail.com>
2023-08-09 19:08:17 +0530

    added: xterm and config

--- packages.sh
+++ packages.sh
@@ -8,10 +8,10 @@ echo "run as USER on first login"
 echo "========================================================"
 # Installs
 sudo pacman -Syy
-sudo pacman -S --needed xorg-server xorg-xinit ntfs-3g noto-fonts noto-fonts-emoji alsa-utils wireguard-tools openresolv wgcf viewnior scrot aria2 yt-dlp python python-brotli mpv clipmenu htop p7zip tealdeer ripgrep exa
+sudo pacman -S --needed xorg-server xorg-xinit ntfs-3g noto-fonts noto-fonts-emoji alsa-utils wireguard-tools openresolv wgcf viewnior scrot aria2 yt-dlp python python-brotli mpv clipmenu htop p7zip tealdeer ripgrep exa xterm
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -sic
 yay -S ungoogled-chromium-bin
-# config files
+# config files / dot files
 mv psd ~/.config/
 mv bashrc ~/.bashrc
 mv mpv ~/.config/
@@ -20,10 +20,10 @@ mv angrysearch ~/.config/
 mv sublime-text ~/.config/
 mv chromium-flags.conf ~/.config/
 mv chrome-flags.conf ~/.config/
+mv Xresources ~/.Xresources
 mkdir ~/Pictures
 
 echo "echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor" >> ~/.bash_profile
-echo "[+] PERFORMANCE MODE ON"
 echo "startx" >> ~/.bash_profile
 echo ""
 echo 'Finished.'
