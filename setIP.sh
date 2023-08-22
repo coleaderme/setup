ce7d1ba6317502932af8e080e9b293e71d8fb3ac
coleaderme <coleaderme@gmail.com>
2023-08-22 20:36:23 +0530

    note: manual ip address.

--- setIP.sh
+++ setIP.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
-
 ## Set manual ip address, resets on reboot.
-## usage:
-#       ./setIP.sh eth0 192.168.0.110 192.168.0.1
-
-## Clear current settings
-#ip address flush dev $device
-#ip route flush dev $device
-
+# USAGE:
+#      sudo ./setIP.sh eth0 192.168.0.110 192.168.0.1
+#
+ip address flush dev $1
+ip route flush dev $1
 ip address add $2/24 brd + dev $1
 ip route add $3 dev $1
 ip route add default via $3 dev $1
+
+## in case did not work, try...then try again.
+#sudo ip link set enp2s0 down
+#sudo ip link set enp2s0 up
