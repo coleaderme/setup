ef5d476639e3e130853850f38236586b649e4fe5
coleaderme <coleaderme@gmail.com>
2023-08-25 17:04:34 +0530

    added: macchanger script

--- setIP.sh
+++ setIP.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/bash
-## Set manual ip address, resets on reboot.
-# USAGE:
-#      sudo ./setIP.sh eth0 192.168.0.110 192.168.0.1
-#
-ip link set $1 up
+if [[ $1 == '' ]]
+then
+    echo -e "USAGE:\n ./setIP.sh eth0 192.168.0.110 192.168.0.1\n\n(resets on reboot)"
+    exit
+fi
 ip address flush dev $1
 ip route flush dev $1
 ip address add $2/24 brd + dev $1
 ip route add $3 dev $1
 ip route add default via $3 dev $1
-
-## in case did not work, try...then try again.
-#sudo ip link set enp2s0 down
-#sudo ip link set enp2s0 up
---
+++ setMac.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+if [[ $1 == '' ]]
+then
+    echo -e "USAGE:\n ./setMac.sh eth0 XX:XX:XX:XX:XX\n\n(resets on reboot)"
+    exit
+fi
+sudo ip link set $1 down
+sudo macchanger --mac $2
+sudo ip link set $1 up
