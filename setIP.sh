63c5fbb19a56cc78d87f89c06e2aecf8b669c0f8
coleaderme <coleaderme@gmail.com>
2023-08-26 17:37:27 +0530

    fix: setIP.sh

--- setIP.sh
+++ setIP.sh
@@ -4,6 +4,7 @@ then
     echo -e "USAGE:\n ./setIP.sh eth0 192.168.0.110 192.168.0.1\n\n(resets on reboot)"
     exit
 fi
+ip link set $1 up
 ip address flush dev $1
 ip route flush dev $1
 ip address add $2/24 brd + dev $1
