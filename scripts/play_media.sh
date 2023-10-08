fc3594f892566eb88ac66cabd8d88259e8759054
coleaderme <coleaderme@gmail.com>
2023-10-08 20:11:08 +0530

    update::search.sh::mpv can play gif!

--- scripts/play_media.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-f="$(fd -tf -e mp3 -e opus -e m4a -e flac -e wav -e mkv -e webm -e mp4 -e m4v -a | dmenu -i -l 20 -fn InconsolataSemiCondensedBold)"
-[ -z $f ] || mpv --replaygain=album --no-audio-display "$f"
-# empty OR not empty - simple.
-# if file is empty, don't do anything.
-# otherwise pass it to mpv.
--- scripts/search.sh
+++ scripts/search.sh
@@ -2,10 +2,14 @@
 # .config/fd/ignore <= items to ignore.
 #
 f="$(fd -H | dmenu -p "_>" -i -l 30 -fn InconsolataSemiCondensedBold)"
+killall fd
+killall dmenu
 [ -z "$f" ] && exit
-## case is amazing!!
 case "$f" in
-  *\.sh|*\.py|*\.c|*\.h|*\.rs|*\.js) subl "$f" ;;
-  *\.flac|*\.m4a|*\.ogg|*\.opus|*\.mp3|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv "$f" ;;
-  *)     thunar "$f" ;;
+  *\.sh|*\.py|*\.c|*\.h|*\.rs|*\.js|*\.txt|*\.toml|*\.conf|*rc) subl "$f" ;;
+  # *\.flac|*\.m4a|*\.gif|*\.ogg|*\.opus|*\.mp3|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv "$f" ;;
+  *\.gif|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv --no-audio-display --replaygain=album "$f" ;;
+  *\.flac|*\.m4a|*\.ogg*\.opus|*\.mp3) audacious "$f" ;;
+  *\.jpg|*\.png|*\.webp) viewnior "$f" ;;
+  *) thunar "$f" ;;
 esac
