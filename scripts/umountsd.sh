19c744144314c4c626694849570a679b080ed8ad
coleaderme <coleaderme@gmail.com>
2023-10-09 13:28:19 +0530

    fix: umountsd.sh

--- scripts/umountsd.sh
+++ scripts/umountsd.sh
@@ -2,10 +2,11 @@
 # usage:
 #   ./mountsd.sh sdc1
 # old # sudo mount /dev/$1 ~/sd/ -o noatime,umask=000
-d=$(lsblk -n -l -o NAME,SIZE,FSTYPE,LABEL,MOUNTPOINTS | dmenu -p "External Drive:" -l 12 -fn InconsolataSemiCondensedBold | choose -1)
+d=$(lsblk -n -l -o NAME,SIZE,FSTYPE,LABEL,MOUNTPOINTS | dmenu -p "Eject Drive:" -l 12 -fn InconsolataSemiCondensedBold | choose -1)
 [ -z $d ] && exit
 case "$d" in
-    /) exit ;;
-    /home) exit ;;
-    /home/*) sudo umount "$d" ;;
+    \/) exit;;
+    \/home) exit;;
+    \/home\/*) sudo umount $d;;
+    *) echo "$d" | dmenu -p "[-] Invalid Eject Drive: ";;
 esac
