245479ed362378957d9985af83bee5b55607eeb0
coleaderme <114289459+coleaderme@users.noreply.github.com>
2023-10-23 15:13:17 +0530

    do not run as sudo

--- startx_dwm.sh
+++ startx_dwm.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/sh
+# clone as user, run as user.
+# Do NOT run as sudo.
+#just: sh startx_dwm.sh
 sudo pacman -S feh
 # install dwm
 git clone https://github.com/coleaderme/dwm.git && cd dwm && sudo make install -j2
@@ -11,7 +14,7 @@ cd st
 sudo make install -j2
 cd ..
 # wallpaper
-sudo mkdir /usr/share/backgrounds/xfce
+sudo mkdir -p /usr/share/backgrounds/xfce
 sudo mv wallpaper/Supernatural-Wallpaper.png /usr/share/backgrounds/xfce/Supernatural-Wallpaper.png
 sudo mv startx_xyz/xinitrc_dwm /etc/X11/xinit/xinitrc
 echo "You may reboot now."
