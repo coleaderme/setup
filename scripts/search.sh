a6b56427fcfdf472d786f0304e6bab11d7d1ca80
coleaderme <coleaderme@gmail.com>
2023-11-07 13:33:45 +0530

    [update] new context menu: search.sh

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/sh
 # .config/fd/ignore <= items to ignore.
 #
-f="$(fd -H | dmenu -p "_>" -i -l 30 -fn InconsolataSemiCondensedBold)"
+f="$(fd | dmenu -p "_>" -i -l 35 -fn InconsolataSemiCondensedBold)"
 killall fd
 killall dmenu
-[ -z "$f" ] && exit
-case "$f" in
-  *\.sh|*\.py|*\.c|*\.h|*\.rs|*\.js|*\.txt|*\.toml|*\.conf|*rc) subl "$f" ;;
-  # *\.flac|*\.m4a|*\.gif|*\.ogg|*\.opus|*\.mp3|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv "$f" ;;
-  *\.gif|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv --no-audio-display --replaygain=album "$f" ;;
-  *\.flac|*\.m4a|*\.ogg*\.opus|*\.mp3) audacious "$f" ;;
-  *\.jpg|*\.png|*\.webp) viewnior "$f" ;;
-  *) thunar "$f" ;;
+[ -z "$f" ] && exit # exit if none selected.
+
+action=$(echo "open\nopen in explorer\nrename\ndelete" | dmenu -p "_>" -i -l 35 -fn InconsolataSemiCondensedBold)
+[ -z "$action" ] && exit # exit if none selected.
+
+open(){
+case "$1" in
+    *\.sh|*\.py|*\.c|*\.h|*\.rs|*\.js|*\.txt|*\.toml|*\.conf|*rc|*\.md) subl "$1" ;;
+    *\.gif|*\.webm|*\.mkv|*\.mp4|*\.m4v) mpv --no-audio-display --volume=85 --replaygain-clip=yes --replaygain=album "$1" ;;
+    *\.flac|*\.m4a|*\.ogg|*\.opus|*\.mp3) audacious ;;
+    *\.jpg|*\.png|*\.webp) viewnior "$1" ;;
+    *) echo "Unknown filetype: '$1'" | dmenu -p "_>" -l 1 -fn InconsolataSemiCondensedBold ;;
+esac
+}
+
+open_in_explorer(){
+    thunar "$1"
+}
+
+rename(){
+    new_name=$(echo "$1" | dmenu -p "Rename as:" -l 1 -fn InconsolataSemiCondensedBold)
+    mv "$1" "$new_name"
+}
+
+delete(){
+    echo "$1" | dmenu -p "Deleted:" -l 1 -fn InconsolataSemiCondensedBold
+    rm "$1"
+}
+
+case "$action" in
+    open) open "$f" ;;
+    *explorer) open_in_explorer "$f" ;;
+    rename) rename "$f" ;;
+    delete) delete "$f" ;;
 esac
---
+++ scripts/ytx.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+yt-dlp -x --restrict-filenames "$(xsel -o)"
