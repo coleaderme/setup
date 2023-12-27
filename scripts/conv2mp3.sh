be904fbe2f3219ff12e55a4b93ca478641368032
coleaderme <coleaderme@gmail.com>
2023-12-27 19:54:36 +0530

    minor changes

--- scripts/conv2mp3.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-for f in ./inputf/*.flac; do
-    # echo ffmpeg -i \""$f"\" -acodec libmp3lame -b:a 320k \""${f%.*}.mp3\""
-    ffmpeg -i "$f" -acodec libmp3lame -b:a 320k "${f%.*}".mp3
-done
