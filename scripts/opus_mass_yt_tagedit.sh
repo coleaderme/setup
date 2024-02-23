fc533dc77e37d0a62f9e5f0956943016f252ba9e
coleaderme <coleaderme@gmail.com>
2024-02-24 01:05:36 +0530

    opustags: updated

--- scripts/opus_mass_yt_tagedit.sh
+++ scripts/opus_mass_yt_tagedit.sh
@@ -2,9 +2,10 @@
 # Mass tag editor for opus YT rips.
 # best for mass editing for single artist/album
 #
-for f in ./*.opus
-do 
-    title="$(echo "${f%.*}" | cut -b3-)"              ##<<----- removes "./" & ".opus"
-    opustags -a title="$title" -i "$f"
+for f in *.sh
+do
+    title="${f%.*}"                                   ##<<----- removes  file.ext
+    echo "$title"
+    # opustags -a title="$title" -i "$f"
     # opustags -a artist="FarooqGotAudio" -i "$f"     ##<<------ can be used on same line.
 done
