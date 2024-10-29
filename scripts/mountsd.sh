19e24b50e2be182b39bee6c443634dec05621744
coleaderme <coleaderme@gmail.com>
2024-10-29 20:14:09 +0530

    mount.sh fix

--- scripts/mountsd.sh
+++ scripts/mountsd.sh
@@ -4,6 +4,7 @@ main(){
     d=$(lsblk -n -l -o NAME,SIZE,FSTYPE,LABEL,MOUNTPOINTS | dmenu -p "Mount Drive:" -i -l 12 | choose 0)
     [ -z "$d" ] && exit # exit if empty
     mountpoint=$(printf "D\nE\nF\nG\n" | dmenu -p "mount point: " -i -l 4)
+    [ -z "$mountpoint" ] && exit # exit if empty
     mkdir ~/"$mountpoint"
     sudo mount "/dev/$d" ~/"$mountpoint" -o noatime,umask=000
     herbe "Mounted: $d to ~/$mountpoint"
