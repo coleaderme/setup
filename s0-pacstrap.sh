8639e982772ffbe38662b2a56e060ba80617f489
coleaderme <coleaderme@gmail.com>
2025-04-07 21:23:21 +0530

    optional: intel packages

--- s0-pacstrap.sh
+++ s0-pacstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 # sh pacstrap.sh
 #
-# Intel cpu: intel-ucode
+# Intel cpu: intel-ucode (optionals: xf86-video-intel vulkan-intel)
 # AMD   cpu: amd-ucode
 #
 pacstrap -K /mnt base base-devel sudo linux linux-firemware linux-headers fzf sd nano git intel-ucode
