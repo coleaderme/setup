d889a289aa0e31b64ec08cfab99ec3af502a4d31
coleaderme <coleaderme@gmail.com>
2025-04-27 21:51:20 +0530

    removed: unused pacman.conf, added: makepkg.conf

--- s1-base_install.sh
+++ s1-base_install.sh
@@ -39,6 +39,7 @@ echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain localhost" >> /etc/hosts
 ## etc confs
 cp etc/nanorc /etc/
+cp etc/makepkg.conf /etc/
 # cp etc/asound.conf /etc/ # copy if using alsa eq
 cp etc/resolv.conf /etc/
 cp etc/dnscrypt-proxy.toml /etc/dnscrypt-proxy/
