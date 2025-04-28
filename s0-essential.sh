9c06d4943fb408150e70c2bce7be16ad1356693a
coleaderme <coleaderme@gmail.com>
2025-04-28 18:57:04 +0530

    rankmirror arguments

--- s0-essential.sh
+++ s0-essential.sh
@@ -10,5 +10,5 @@ pacman -Syy
 pacman -S --needed --noconfirm pacman-contrib reflector rsync sd fzf dnscrypt-proxy
 systemctl disable dnscrypt-proxy.socket
 systemctl enable dnscrypt-proxy.service
-reflector --threads 4 --sort rate --country IN --age 6 --download-timeout 5 --fastest 10 --ipv4 --save /etc/pacman.d/mirrorlist
+reflector --threads 4 --sort rate --download-timeout 5 --country IN --age 6 --protocol http --fastest 10 --ipv4 --save /etc/pacman.d/mirrorlist
 echo 1 >> already_ran_reflector.txt
