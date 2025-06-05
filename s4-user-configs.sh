eb796b72653f5c13aa3cc7ecfd80fd8e85acef7d
coleaderme <coleaderme@gmail.com>
2025-06-05 19:05:41 +0530

    bold font for applications

--- s4-user-configs.sh
+++ s4-user-configs.sh
@@ -11,6 +11,7 @@ mkdir ~/.config
 cp config/*.conf ~/.config/
 cp config/mimeapps.list ~/.config
 cp -rf config/psd ~/.config/
+cp -rf config/gtk-3.0 ~/.config/
 cp -rf config/bashrc ~/.bashrc
 cp -rf config/Xresources ~/.Xresources
 cp -rf config/nanorc ~/.nanorc
