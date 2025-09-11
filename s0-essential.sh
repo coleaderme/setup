7414e8ed3fc1b2ef2f4b3abe10066a04f2e2d7e6
coleaderme <coleaderme@gmail.com>
2025-09-11 22:06:47 +0530

    remove sd

--- s0-essential.sh
+++ s0-essential.sh
@@ -7,7 +7,7 @@ echo "================================"
 echo "+++Installing essential tools+++"
 echo "================================"
 pacman -Syy
-pacman -S --needed --noconfirm pacman-contrib reflector rsync sd fzf dnscrypt-proxy
+pacman -S --needed --noconfirm pacman-contrib reflector rsync fzf dnscrypt-proxy
 systemctl disable dnscrypt-proxy.socket
 systemctl enable dnscrypt-proxy.service
 reflector --threads 4 --sort rate --download-timeout 5 --country IN --age 6 --protocol http --fastest 10 --ipv4 --save /etc/pacman.d/mirrorlist
