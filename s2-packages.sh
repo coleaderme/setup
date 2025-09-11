9f657d5ce4104be753653d45c61aa2b0751f59c5
coleaderme <coleaderme@gmail.com>
2025-09-11 23:51:30 +0530

    fixes

--- s2-packages.sh
+++ s2-packages.sh
@@ -5,7 +5,7 @@
 pacman -Syy
 pacman -S --needed --noconfirm xorg-server xorg-xinit xorg-xset ntfs-3g \
 ttf-arphic-ukai ttf-indic-otf ttf-ubuntu-font-family noto-fonts-emoji \
-alsa-utils wireguard-tools openresolv wgcf viewnior xsel scrot \
+alsa-utils wireguard-tools openssh openresolv wgcf viewnior xsel scrot \
 aria2 mpv polkit htop 7zip \
 ripgrep fd xh fzf skim choose numlockx \
 earlyoom thunar eza micro nnn feh miniserve dnscrypt-proxy
