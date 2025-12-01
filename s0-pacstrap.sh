f8bb26c417636fac22f195df1adb3b4c64b7304f
coleaderme <coleaderme@gmail.com>
2025-12-01 19:55:06 +0530

    re-structured

--- s0-pacstrap.sh
+++ s0-pacstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 #
-# Intel cpu: intel-ucode (optionals: xf86-video-intel vulkan-intel)
+# Intel cpu: intel-ucode (optionals: libva-intel-driver xf86-video-intel vulkan-intel)
 # AMD   cpu: amd-ucode
 #
 pacstrap -K /mnt base base-devel sudo linux-lts linux-lts-headers linux-firmware fzf nano git intel-ucode
-genfstab -U /mnt > /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
--- s2-packages.sh
+++ s2-packages.sh
@@ -9,7 +9,7 @@ alsa-utils wireguard-tools openssh openresolv wgcf viewnior xsel scrot \
 aria2 mpv cmus polkit htop 7zip \
 ripgrep fd xh fzf skim choose numlockx \
 earlyoom thunar eza micro nnn feh miniserve \
-qrencode dust dnscrypt-proxy rclone fuse3
+android-tools qrencode dust dnscrypt-proxy rclone fuse3
 ## ttyper: Terminal-based typing test
 ## mtr: ping hops traceroute
 ## copyparty: filesharing
@@ -17,11 +17,12 @@ qrencode dust dnscrypt-proxy rclone fuse3
 ## fclones: find duplicates
 ## joshuto-bin (via yay) user friendly! TUI file explorer.
 ## micro: nano++
-## jq jless (for json), uv (for python-pip and venv alternative)
+## jq jless: (for json)
+## uv: (for python-pip and venv alternative)
 ## fend (calc) [from yay/github]
 ## herbe (minimal popup notification) [from github]
 ## code-oss (vscode opensource build) with exts -> ruff + python + cody (free ver)
-## android-tools gvfs gvfs-mtp (android device support)
+## gvfs gvfs-mtp: (auto mount various devices)
 ## woeusb: create windows usb
 ## usbimager: create linux usb
 ## gitui: tui for git
